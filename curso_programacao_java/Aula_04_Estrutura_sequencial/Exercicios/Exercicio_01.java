ccf76416152360e3c5f5bd1c809617deb1aef015
JoaoHSantana2007 <jhfcsant2007@gmail.com>
2025-08-18 11:36:24 -0300

    add exercicios

--- curso_programacao_java/Aula_04_Estrutura_sequencial/Exercicios/Exercicio_01.java
+++ curso_programacao_java/Aula_04_Estrutura_sequencial/Exercicios/Exercicio_01.java
@@ -1,10 +1,26 @@
 package Exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio_01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n1, n2, soma;
+		
+		System.out.println("Digite o valor de n1: ");
+		n1 = sc.nextInt();
+		
+		System.out.println("Digite o valor de n2: ");
+		n2 = sc.nextInt();
+		
+		soma = n1 + n2;
+		
+		System.out.println("O resulrtado de soma eh: " + soma);
+		
+		sc.close();
 	}
 
 }
