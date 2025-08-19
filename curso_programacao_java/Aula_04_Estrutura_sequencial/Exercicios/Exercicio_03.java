a2eda873e1dd8c5dfb3deb6ff6172787749fa53a
JoaoHSantana2007 <jhfcsant2007@gmail.com>
2025-08-19 08:22:29 -0300

    att

--- curso_programacao_java/Aula_04_Estrutura_sequencial/Exercicios/Exercicio_03.java
+++ curso_programacao_java/Aula_04_Estrutura_sequencial/Exercicios/Exercicio_03.java
@@ -1,11 +1,28 @@
 package Exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio_03 {
 
 	public static void main(String[] args) {
-	
+
+		Scanner sc = new Scanner(System.in);
 		
+		int a, b, c, d, diferenca;
+
+		System.out.println("Digite os valores corespondentes: ");
 		
+	    a = sc.nextInt();
+	    b = sc.nextInt();
+	    c = sc.nextInt();
+	    d = sc.nextInt();
+
+	    diferenca = a * b - c * d;
+
+	    System.out.println("DIFERENCA = " + diferenca);	
+		
+		sc.close();
+
 	}
 
 }
